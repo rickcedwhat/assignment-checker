9874449b3551c3995ede1d9a5bd4e28acf786f74
Cedrick Catalan <ccata002@gmail.com>
2025-09-16 20:35:56 -0400

    fix: Remove trailing slash from Firebase web app URL in CORS origins

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://localhost:3000",
-    "https://vip-hw.web.app/"
+    "https://vip-hw.web.app"
 ]
 
 app.add_middleware(
